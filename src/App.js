5d274bc349a67db2b05f0634dc1892c5c06eeeed
SKY <88556477+SKY1272@users.noreply.github.com>
2023-06-14 03:59:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SearchEmoji from "./components/SearchEmoji";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SearchEmoji />
+    </div>
+  );
+}
